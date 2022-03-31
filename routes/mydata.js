8a4e0a8ec4ea0972710735fa54314f788e5d7cf2
reddyvp <reddyvamsidharp@gmail.com>
2022-03-30 19:38:06 -0500

    bug modified in app.js

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.mydata('index', { title: 'Vamsidhar reddy' });
+  res.render('index', { title: 'VAmsidhar reddy' });
 });
 
 module.exports = router;
