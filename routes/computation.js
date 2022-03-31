2a292b7ca2d39137fb3bbd3e833b002cb857d1ba
reddyvp <reddyvamsidharp@gmail.com>
2022-03-30 19:45:16 -0500

    Create computation.js

---
+++ routes/computation.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET computation page. */
+router.get('/', function (req, res, next) {
+  var x;
+  //var random = Math.random() * (3) + (-1);
+  var random = Math.random();
+  console.log(req.query.x);
+  x = req.query.x;
+
+  // checking if url has params
+  if (x == undefined) {
+    x = random;
+  }
+
+
+  
+  let a = Math.cos(x,x) 
+  let b =Math.asin(x)
+  let c = Math.asinh(x)
+  res.render('computation', {
+    title: 'Bonus',
+    values: `applied to ` + x + ` is ` + cos ,
+    v1: `applied to ` + x + ` is ` + asin,
+    v2: `applied to ` + x + ` is ` + asinh
+  });
+});
+
+module.exports = router;
